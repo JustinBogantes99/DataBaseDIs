12e5678d3b73023ac39d9799396bf70357f47191
Raydertrent <50339548+Raydertrent@users.noreply.github.com>
2021-06-22 10:50:43 -0600

    Actualización del modelo de Sala
    -Se agrega el atributo "estado" a las clases contenidas en Sala

--- models/sala.model.js
+++ models/sala.model.js
@@ -9,7 +9,7 @@ const salaSchema = new Schema({
     clases:[{nombre:String, capacidadMaxima:Number, diaEjecucion:String, instructor:{cedula:String, nombreCompleto:String, celular:Number, correo:String, TRol:String,
              estado:Boolean, cuenta:{nombreUsuario:String,contrasenia:String}, servicios:{nombre:String, precio:Number, maximoPersonas:Number}}, 
             servicio:{nombre:String, precio:Number, maximoPersonas:Number}, horaInicio:{type:String}, horaFin:{type:String}, precio: {type:Number},
-            pagos:[{fechaPago:String, clienteNombreUsuario:String, monto:Number, estado:String, metodoPago:String}]}],
+            pagos:[{fechaPago:String, clienteNombreUsuario:String, monto:Number, estado:String, metodoPago:String}], estado:String}],
     servicios:[{nombre:String, precio:Number, maximoPersonas:Number}]
 })
 
